29f9b737290abbb3a2041c5278ecfe7e06627b42
Dasmat13 <mey37056@gmail.com>
2025-07-18 13:43:25 +0530

    new file:   main.tf 	new file:   terraform.tfstate 	new file:   variables.tf

---
+++ terraform_input_variables/variables.tf
@@ -0,0 +1,19 @@
+variable "filename" {
+    default = "/root/pets.txt"
+}
+
+variable "content" {
+    default = "my lovely pet"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable "length" {
+    default = "1"
+}
