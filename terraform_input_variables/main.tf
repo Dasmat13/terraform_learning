45316f38c6e2c981b6b77cdb9648eccf4288f9a9
Dasmat13 <mey37056@gmail.com>
2025-07-18 16:32:26 +0530

    new file:   terraform_input_variables/terraform.tfstate.backup

--- terraform_input_variables/main.tf
+++ terraform_input_variables/main.tf
@@ -9,3 +9,18 @@ resource "random_pet" "my-pet" {
     length = var.length
   
 }
+
+#lsit example
+
+resource "random_pet" "list" {
+    prefix = var.prefix1[1]
+    separator = var.separator
+    length = var.length
+}
+
+#map example 
+
+resource "local_file" "my-map" {
+    filename = "/root/map.txt"
+    content = var.file-content["statement1"]
+}
--- terraform_input_variables/variables.tf
+++ terraform_input_variables/variables.tf
@@ -17,3 +17,42 @@ variable "separator" {
 variable "length" {
     default = "1"
 }
+
+##list 
+
+variable "prefix1" {
+    default = ["Mr","Mrs","Sir"]
+    type = list(string)
+}
+
+##map 
+
+variable "file-content" {
+    type = map
+    default = {
+        "statement" = "we love pets"
+        "statement2" = "we love animals"
+    }
+}
+
+##set: the difference between set and list is a set cannot have duplicate elements
+
+##object 
+
+ variable "bella" {
+    type = object({
+      name = string
+      color = string
+      age = number
+      food = list(string)
+      favourite_pet = bool 
+    }) 
+ }
+
+ ##tuples
+
+ variable "kitty" {
+    type = tuple([ string, number, bool ])
+    default = [ "cat", 1, false ]
+   
+ }
