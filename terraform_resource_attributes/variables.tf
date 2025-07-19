0b26e5e992790593bcf9ff000d370989ca702839
Dasmat13 <mey37056@gmail.com>
2025-07-19 14:39:46 +0530

    added resource file attribute

---
+++ terraform_resource_attributes/variables.tf
@@ -0,0 +1,20 @@
+variable "filename" {
+    default = "/home/dasmat/pets.txt"
+}
+
+variable "content" {
+    default = "my lovely pet"
+}
+
+variable "prefix" {
+    default = "Mrs"
+}
+
+variable "separator" {
+    default = "."
+}
+
+variable "length" {
+    default = "1"
+}
+
